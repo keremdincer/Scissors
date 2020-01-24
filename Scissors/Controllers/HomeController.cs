e8cda93b80f542ab80e86f1fd1394a919e02f489
Kerem Dincer <b.keremdincer@gmail.com>
2020-01-24 11:08:49 +0300

    Create redirector controller
    Create a controller for redirecting short urls to original urls.

---
+++ Scissors/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Scissors.Services;
+using Scissors.Dtos;
+
+namespace Scissors.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var url = Request.Url.PathAndQuery.Substring(1);
+
+            if (string.IsNullOrEmpty(url))
+            {
+                // Not Found'a yönlendir.
+                return View("NotFound");
+            }
+
+
+            var shortUrl = ShortUrlService.FindByUrl(url);
+            if (shortUrl == null)
+            {
+                // Not Found'a yönlendir.
+                return View("NotFound");
+            }
+
+            // ShortUrl için log oluştur.
+            // Bağlanan cihazla ilgili bilgileri kaydet.
+            var usageDto = new ShortUrlUsageDto() {
+                ShortUrlId = shortUrl.Id,
+                ClientIP = Request.UserHostAddress,
+                ClientBrowser = Request.Browser.Browser,
+                ClientDevice = Request.Browser.IsMobileDevice ? "Mobile" : "Desktop",
+                ClientOS = Request.Browser.Platform
+            };
+
+            try
+            {
+                ShortUrlService.AddUsage(usageDto);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: On adding usage info.");
+            }
+
+            return Redirect(shortUrl.OriginalUrl);
+        }
+    }
+}
