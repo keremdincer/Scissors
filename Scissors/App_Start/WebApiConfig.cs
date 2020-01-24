a451ed3ba501028776c6932cf3749a62857f8505
Kerem Dincer <b.keremdincer@gmail.com>
2020-01-24 11:00:44 +0300

    Initial commit

---
+++ Scissors/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Scissors
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            /** 
+             * Xml yerine Json cevap döndürmesi için
+             * Eğer web üzerinde görünlemek gerekirse açılabilir
+             */
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
