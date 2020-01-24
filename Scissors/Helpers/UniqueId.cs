77f1cbda9a11cbd5d4ccaadbc3bd1870066f4734
Kerem Dincer <b.keremdincer@gmail.com>
2020-01-24 11:03:48 +0300

    Create short unique id generator

---
+++ Scissors/Helpers/UniqueId.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Scissors.Helpers
+{
+    public class UniqueId
+    {
+        public static string Generate (int size = 8)
+        {
+            char[] chars = new char[62];
+            string domain = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            chars = domain.ToCharArray();
+
+            byte[] data = new byte[1];
+
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+
+            StringBuilder result = new StringBuilder(size);
+
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length - 1)]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
